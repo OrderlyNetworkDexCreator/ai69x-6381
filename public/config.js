3d14843d1d8e3a320282ca0d5ded6d02d341a389
Mario Reder <mario.reder@pm.me>
2026-06-01 22:21:59 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Portfolio,Trading,Markets,Rewards,Points",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "Ecosystem, https://www.fartprint.art/; Printr, https://www.fartprint.art/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/FARTPRINT",
